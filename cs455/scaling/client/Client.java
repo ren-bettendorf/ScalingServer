dce132d0ae1dfbdd3c3a9fa874fd9ea00e6e568b
Ren Bettendorf <rbetten@madison.cs.colostate.edu>
2017-02-22 10:48:30 -0700

    Added more functionality for starting Client

--- cs455/scaling/client/Client.java
+++ cs455/scaling/client/Client.java
@@ -1,7 +1,9 @@
 package cs455.scaling.client;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.util.LinkedList;
@@ -11,16 +13,45 @@ import java.util.Random;
 public class Client {
 	private LinkedList<String> hashcodes;
 	private Selector selector;
-	private int buffSize = 8000;
+	private final int buffSize = 8000;
+	private final int messageRate;
 
-	public Client(String serverHost, int port) throws IOException {
+	public Client(String serverHost, int port, int messageRate) throws IOException {
 		this.hashcodes = new LinkedList<String>();
 		startClient(serverHost, port);
+		this.messageRate = messageRate;
 	}
 
 	public static void main(String[] args) {
-		System.out.println("NARP");
+		if(args.length != 3) {
+                        System.out.println("Arguments not correct server-address server-port message-rate");
+                        System.exit(0);
+                }
+		String serverHost = args[0];
+                int serverPort = -1;
+                int messageRate = -1;
+
+                try {
+                        serverPort = Integer.parseInt(args[1]);
+                        messageRate = Integer.parseInt(args[2]);
+                } catch(NumberFormatException nfe) {
+                        System.out.println("Something went wrong with parsing port and message rate");
+                        System.exit(0);
+                }
+                try {
+                        System.out.println("Starting client: " + InetAddress.getLocalHost().getHostAddress());
+                } catch (UnknownHostException uhe) {
+                        System.out.println("Something went wrong with localhost");
+                }
+                try{
+                        new Client(serverHost, serverPort, messageRate);
+                }catch (IOException ioe) {
+                        System.out.println("IOException: Exiting program");
+                        ioe.printStackTrace();
+                        System.exit(0);
+                }
 	}
+
 	public void startClient(String serverHost, int serverPort) throws IOException {
 		this.selector = Selector.open();
 
