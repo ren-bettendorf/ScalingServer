9b4632c559378bd24b9c834074403a24c0a129f7
Ren Bettendorf <rbetten@madison.cs.colostate.edu>
2017-02-24 20:47:32 -0700

    Added random 10 second sleep to make sure that threads are multithreading

--- cs455/scaling/threads/TestThreadPool.java
+++ cs455/scaling/threads/TestThreadPool.java
@@ -9,7 +9,7 @@ public class TestThreadPool {
 	public static void main(String[] args) {
 		ThreadPoolManager manager = new ThreadPoolManager();
 		for(int i = 0; i < 100; i++) {
-			manager.addTask(new DummyTask(i, (new Random()).nextInt(1001)));
+			manager.addTask(new DummyTask(i, (new Random()).nextInt(10001)));
 		}
 		manager.initializeThreadPool(10);
 		manager.startTask();
