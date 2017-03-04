deba0bed95d5a3b2d98f732393792054eea1124e
Ren Bettendorf <rbetten@dover.cs.colostate.edu>
2017-03-03 18:09:16 -0700

    Added a message tracker that can be used by Client and Server

---
+++ cs455/scaling/util/MessageTracker.java
@@ -0,0 +1,47 @@
+package cs455.scaling.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class MessageTracker implements Runnable {
+
+	// Will use to message every 10seconds
+	private final int messageRate;
+	private int messageSent, messageReceived;
+	private final Object lock = new Object();
+	private final DateTimeFormatter formatter;
+	public MessageTracker(int messageTime) {
+		this.messageSent = 0;
+		this.messageReceived = 0;
+		this.messageRate = messageTime;
+		formatter = DateTimeFormatter.ofPattern("MM/dd HH:mm:ss");
+	}
+
+	public void incrementMessageSent() {
+		synchronized(lock) {
+			messageSent++;
+		}
+	}
+
+	public void incrementMessagesReceived() {
+		synchronized(lock) {
+			messageReceived++;
+		}
+	}
+
+	@Override
+	public void run() {
+		LocalDateTime messageTime = LocalDateTime.now().plusSeconds(messageRate);
+		while(true) {
+			LocalDateTime current = LocalDateTime.now();
+			if(current.isAfter(messageTime)) {
+				messageTime = current.plusSeconds(messageRate);
+				synchronized(lock) {
+					System.out.println("[" + messageTime.format(formatter) + "] Total Sent Count: " + messageSent + ", Total Received Count: " + messageReceived );
+					messageSent = 0;
+					messageReceived = 0;
+				}
+			}
+		}
+	}
+}
